5f547277423be2850bdd9cf6d32a25842bbf3861
juli20008 <mia.li.mba@gmail.com>
2023-07-06 00:44:25 -0400

    netlify

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ const Users=Models.User;
 const Genres=Models.Genre;
 const Directors=Models.Director;
 const cors = require('cors');
-app.use(cors({ origin: '*' }))
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://mysuperflix.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Middleware
 app.use(express.static('public'));
